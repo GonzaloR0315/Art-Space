f4523f67fa26c46779f9a443823821c52c425f65
hacking-jack <daniel.rodriguezti1@gmail.com>
2023-10-09 21:15:16 +0200

    Presuntamente terminado deberia funcionar no puedo probar

--- app/src/main/java/com/example/art_space/MainActivity.kt
+++ app/src/main/java/com/example/art_space/MainActivity.kt
@@ -21,31 +21,74 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.art_space.R
+import com.example.art_space.ui.theme.ArtSpaceTheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    var result = 1
-                    printImage(result)
-                }
+            ArtSpaceTheme {
+                ArtSpaces()
 
+            }
         }
     }
 }
+//
+//@Composable
+//fun printImage(option: Int){
+//    var result by remember { mutableStateOf(option) }
+//    var image = painterResource(R.drawable.las_meninas_01)
+//    var titl = ""
+//    var artis = ""
+//    var dat = 0
+//    when(option){
+//        1 ->{
+//            image = painterResource(R.drawable.las_meninas_01)
+//            titl =  stringResource(R.string.title1)
+//            artis = stringResource(R.string.artist1)
+//            dat = 1656}
+//        2 ->{
+//            image = painterResource(R.drawable.persistencia_2)
+//            titl = stringResource(R.string.title2)
+//            artis = stringResource(R.string.artist2)
+//            dat = 1931}
+//        3 ->{
+//            image = painterResource(R.drawable.licensed_image)
+//            titl = stringResource(R.string.title3)
+//            artis = stringResource(R.string.artist3)
+//            dat = 1506}
+//    }
+//
+//    Column(
+//        modifier = Modifier.padding(40.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+////        verticalArrangement = Arrangement.Center
+//    ) {
+//        Image(
+//            painter = image,
+//            contentDescription = null,
+//        )
+//    }
+//    Button(onClick = { result = (1..6).random() }) {
+//        Text(stringResource(R.string.roll))
+//
+//    }
+//    artSpace( titl, artis, dat)
+//}
+//
+//
+
 
 @Composable
-fun printImage(option: Int){
-    var result by remember { mutableStateOf(option) }
+fun printImage(modifier: Modifier){
+    var result by remember { mutableStateOf(1) }
     var image = painterResource(R.drawable.las_meninas_01)
     var titl = ""
     var artis = ""
     var dat = 0
-    when(option){
+    when(result){
         1 ->{
             image = painterResource(R.drawable.las_meninas_01)
             titl =  stringResource(R.string.title1)
@@ -66,52 +109,72 @@ fun printImage(option: Int){
     Column(
         modifier = Modifier.padding(40.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+//        verticalArrangement = Arrangement.Center
     ) {
         Image(
             painter = image,
             contentDescription = null,
         )
-    }
-    artSpace( titl, artis, dat)
-}
-
-
-
-
-@Composable
-fun artSpace( title: String, artist: String, fecha: Int, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier.padding(40.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
 
         Text(
-            text = title,
+            text = titl,
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
-            modifier = modifier
         )
         Row(){
             Text(
-                text = artist,
+                text = artis,
                 fontSize = 12.sp,
-                modifier = modifier
             )
             Text(
-                text = " $fecha",
+                text = " $dat",
                 fontStyle = FontStyle.Italic,
                 fontSize = 12.sp,
-                modifier = modifier
             )
         }
-    }
+
+    Button(onClick = { result = (1..3).random() }, modifier = Modifier) {
+        Text(stringResource(R.string.roll))
+
+    }}
 }
+
+
+//@Composable
+//fun artSpace( title: String, artist: String, fecha: Int, modifier: Modifier = Modifier) {
+//    Column(
+//        modifier = Modifier.padding(40.dp),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Bottom
+//    ) {
+//
+//        Text(
+//            text = title,
+//            fontWeight = FontWeight.Bold,
+//            fontSize = 20.sp,
+//            modifier = modifier
+//        )
+//        Row(){
+//            Text(
+//                text = artist,
+//                fontSize = 12.sp,
+//                modifier = modifier
+//            )
+//            Text(
+//                text = " $fecha",
+//                fontStyle = FontStyle.Italic,
+//                fontSize = 12.sp,
+//                modifier = modifier
+//            )
+//        }
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun ArtSpaces() {
-        printImage(1)
+        printImage( modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center) )
 
 }
